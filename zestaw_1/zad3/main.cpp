fdf7971c371ffa1226af1885f291cc7940e276fb
lm <michal.lewandowski@pl.transport.bombardier.com>
2017-11-10 15:01:38 +0100

    2

--- zestaw_1/zad3/main.cpp
+++ zestaw_1/zad3/main.cpp
@@ -2,17 +2,32 @@
 
 using namespace std;
 
+#define ILOSCLICZB 1000
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int tablicaLiczb[ILOSCLICZB];
+
+    cout << "Podaj ilosc liczb" << endl;
+    int n = 0;
+    cin >> n;
+
+    cout << "Podaj liczby" << endl;
+    string ciagLiczb;
+    getline(cin, ciagLiczb);
+
+
+    for(int i = 1; i < ILOSCLICZB; i++)
+   {
+        tablicaLiczb[i] = 0;
+    }
+    //string temp;
 
 
 
 
 
-
-
-
+    cin >> n;
 
     return 0;
 }
